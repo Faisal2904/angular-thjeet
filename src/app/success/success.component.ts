b375d0dc819fb3a6eee7a2a72d2863736f134d96
Faisal2904 <ahmad.irfan94@gmail.com>
2019-04-05 19:46:10 +0530

    3 files modified

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,8 +1,29 @@
 import {Component} from '@angular/core'
 @Component({
   selector: 'app-success',
-  templateUrl:'./success.component.html'
+  templateUrl:'./success.component.html',
+  styleUrls:['./success.component.css']
 })
 export class SuccessComponent {
+
+count=0;
+status=false;
+arr=[];
+  toggleMe(event){
+  this.count=this.count+1;
+  this.arr.push(this.count);
+  console.log(this.arr);
+  if(this.count%2!=0)
+  {
+    this.status=true;
+  }
+  else{
+    this.status=false
+  }
+  }
+
+  getColor(x){
+    return x>=5?'blue':'white';
+  }
 
 }
