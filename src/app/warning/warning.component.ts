0c79ab7a7750bfc96fbe0eb037f5432108b81952
Faisal2904 <ahmad.irfan94@gmail.com>
2019-04-05 12:22:16 +0530

    2 files modified

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -3,6 +3,20 @@ import {Component} from '@angular/core'
   selector: 'app-warning',
   templateUrl:'./warning.component.html'
 })
-export class WarningComponent {
+export class WarningComponent implements ngOnInit {
+
+serverName='';
+serverStatus="Server not created";
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  addServer(){
+     this.serverStatus="Server with name " + this.serverName + " created";
+  }
 
 }
